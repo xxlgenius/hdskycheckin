2c6ad71034abbecf5f6cc66a461c6aa73b78b2a6
Hsongquan <hszhangown@gmail.com>
2022-11-23 22:02:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from logging.handlers import RotatingFileHandler
 import argparse
 import schedule
 import mysmtp
@@ -20,14 +21,27 @@ import json
 
 # imgpath = 'check_code.jpg'
 
-logging.basicConfig(filename=os.path.join(
-    os.path.abspath('.'), 'logger.log'), level=logging.INFO)
-
+def setLogger():
+    logger = logging.getLogger(__file__)
+    logger.setLevel(logging.INFO)
+    log_file = "{}.log".format(__file__)
+    os.makedirs(os.path.join("./data",log_file),exist_ok=True)
+    logHandler = RotatingFileHandler(os.path.join("./data",log_file), maxBytes=10*1024*1024, backupCount=1, encoding='UTF-8')
+    logHandler.setLevel(logging.INFO)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.INFO)
+    formatter = logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    consoleHandler.setFormatter(formatter)
+    logHandler.setFormatter(formatter)
+    # add
+    logger.addHandler(logHandler)
+    logger.addHandler(consoleHandler)
+    return logger
 
 def getCheckCode(imageHash, image_get_url):
     imgpath = imageHash + '.jpg'
     abs_imgpath = os.path.join(os.path.abspath('.'), imgpath)
-    print('abs_imgpath = '+abs_imgpath)
+    logger.info('abs_imgpath = '+abs_imgpath)
     im = requests.get(image_get_url)  # 获取验证码和cookies值
     with open(abs_imgpath, 'wb') as f:
         f.write(im.content)
@@ -40,7 +54,7 @@ def getCheckCode(imageHash, image_get_url):
     check_code = cp.binary_captchar(abs_imgpath)
 
     os.remove(abs_imgpath)
-    # print("captcha: " + check_code)
+    # logger.info("captcha: " + check_code)
     return check_code
 
 
@@ -49,16 +63,16 @@ def login():
     files = os.listdir('.')
     cookie_name = 'open_cookies.txt'
     cookie_file_name = os.path.join(os.path.abspath('.'), cookie_name)
-    print(cookie_file_name)
-    print(files)
+    logger.info(cookie_file_name)
+    logger.info(files)
     if cookie_name in files:
-        print("startgetcookies")
+        logger.info("startgetcookies")
         with open(cookie_file_name, 'rb') as f:
             cookies = 'c_secure_uid=OTc5MTU%3D; c_secure_pass=bc42097e5d35d2e87a097ea8ee73890b; c_secure_ssl=eWVhaA%3D%3D; c_secure_tracker_ssl=eWVhaA%3D%3D; c_secure_login=bm9wZQ%3D%3D; UM_distinctid=17cd580f1cac7f-07e079df8ccf22-6373267-1fa400-17cd580f1cbc73; CNZZDATA5476511=cnzz_eid%3D1323648825-1635668847-https%253A%252F%252Fhdsky.me%252F%26ntime%3D1642041735; __cf_bm=jmmWOhHjXxOUMlZBnimsyppdd1aXE4VFtRh5yt3riIw-1642043850-0-ATxCJpV2g7floe/YYiB0qngMgdTcY4LV7DVYH0xUIwX742meTewF2X1pe9Hn8I7ZNAXjbLc+29w7q6T9pTO6GDZftyDYj8b8yjgQkTdFOWPZPrNMIRiONNTn1NXmWa9s5g=='
             # cookies = requests.utils.cookiejar_from_dict(pickle.load(f))
             bt_session = requests.session()
             bt_session.cookies = cookies
-            print("finishgetcookies")
+            logger.info("finishgetcookies")
         try:
             torrent_url = 'https://hdsky.me/torrents.php'
             test = bt_session.get(torrent_url)
@@ -139,43 +153,43 @@ def checkin(session,imageHash):
 '''
 
 def job(userName:str,userPwd:str,headless:bool,web:str,webaddr:str):
-    print(time.ctime()+"/开始执行")
+    logger.info(time.ctime()+"/开始执行")
     loginurl = 'https://hdsky.me/login.php'
     driver = None
     webOptions = None
     if web == "chrome":
-        print(time.ctime()+"/开始注册chrome"+"   /浏览器地址:"+webaddr )
+        logger.info(time.ctime()+"/开始注册chrome"+"   /浏览器地址:"+webaddr )
         webOptions = webdriver.ChromeOptions()
         if(headless):
-            print(time.ctime()+"/无头模式启动")
+            logger.info(time.ctime()+"/无头模式启动")
             webOptions.add_argument('--headless')
             webOptions.add_argument("--disable-gpu")
         #driver = webdriver.Chrome(options=webOptions)
     elif web == "edge":
-        print(time.ctime()+"/开始注册edge"+"   /浏览器地址:"+webaddr )
+        logger.info(time.ctime()+"/开始注册edge"+"   /浏览器地址:"+webaddr )
         webOptions = webdriver.EdgeOptions()
         if(headless):
-            print(time.ctime()+"/无头模式启动")
+            logger.info(time.ctime()+"/无头模式启动")
             webOptions.add_argument('--headless')
             webOptions.add_argument("--disable-gpu")
         #driver = webdriver.ChromiumEdge(options=webOptions)
     elif web == "firefox":
-        print(time.ctime()+"/开始注册firefox"+"   /浏览器地址:"+webaddr)
+        logger.info(time.ctime()+"/开始注册firefox"+"   /浏览器地址:"+webaddr)
         webOptions = webdriver.FirefoxOptions()
         if(headless):
-            print(time.ctime()+"/无头模式启动")
+            logger.info(time.ctime()+"/无头模式启动")
             webOptions.add_argument('--headless')
             webOptions.add_argument("--disable-gpu")
         #driver = webdriver.Firefox(options=webOptions)
-    print(time.ctime()+"/开始连接远程浏览器")
+    logger.info(time.ctime()+"/开始连接远程浏览器")
     driver = webdriver.Remote(command_executor=webaddr,options=webOptions)
-    print(time.ctime()+"/浏览器注册完成")
+    logger.info(time.ctime()+"/浏览器注册完成")
 
 
     driver.delete_all_cookies()
     # 打开网页
     driver.get(loginurl)
-    print(time.ctime()+"/打开目标网页")
+    logger.info(time.ctime()+"/打开目标网页")
     '''
     time.sleep(5)
     # cookies注入
@@ -192,17 +206,17 @@ def job(userName:str,userPwd:str,headless:bool,web:str,webaddr:str):
     logimgurl = driver.find_element(By.XPATH,'//*[@id="nav_block"]/form[2]/table/tbody/tr[4]/td[2]/img').get_attribute('src')
     imageHash = str(logimgurl).split('=')[-1]
     log_code = getCheckCode(imageHash, logimgurl)
-    print(time.ctime()+"/验证码解析完毕：" + log_code)
+    logger.info(time.ctime()+"/验证码解析完毕：" + log_code)
     driver.find_element(By.XPATH,'//*[@id="nav_block"]/form[2]/table/tbody/tr[1]/td[2]/input').send_keys(userName)
     driver.find_element(By.XPATH,'//*[@id="nav_block"]/form[2]/table/tbody/tr[2]/td[2]/input').send_keys(userPwd)
     driver.find_element(By.XPATH,'//*[@id="nav_block"]/form[2]/table/tbody/tr[5]/td[2]/input[1]').send_keys(log_code)
     driver.find_element(By.XPATH,'//*[@id="nav_block"]/form[2]/table/tbody/tr[10]/td/input[1]').click()
-    print(time.ctime()+"/完成登录")
+    logger.info(time.ctime()+"/完成登录")
 
     try:
         driver.find_element(By.XPATH,'//*[@id="showup"]').click()
     except:
-        print(time.ctime()+'/未找到目标元素，已签到')
+        logger.warning(time.ctime()+'/未找到目标元素，已签到')
         
     # 定位并获取验证码src
     try:
@@ -210,28 +224,25 @@ def job(userName:str,userPwd:str,headless:bool,web:str,webaddr:str):
         WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="showupimg"]')))
         imgurl = driver.find_element(By.XPATH,'//*[@id="showupimg"]').get_attribute('src')
     except:
-        print(time.ctime()+"/未找到目标签到图片元素")
+        logger.error(time.ctime()+"/未找到目标签到图片元素")
         sendError(time.ctime()+"/未找到目标签到图片元素")
     else:
         # 获取imgurl按‘=’分割的倒数第一个字符串
         imageHash = str(imgurl).split('=')[-1]
         check_code = getCheckCode(imageHash, imgurl)
-        print(time.ctime()+"/签到验证码解析完毕："+check_code)
-        #print(check_code)
+        logger.info(time.ctime()+"/签到验证码解析完毕："+check_code)
+        #logger.info(check_code)
     try:
         driver.find_element(By.XPATH,'//*[@id="imagestring"]').send_keys(check_code)
         driver.find_element(By.XPATH,'//*[@id="showupbutton"]').click()
-        #print(time.ctime() +"/debug:输入验证码完毕并点击了提交")
+        #logger.info(time.ctime() +"/debug:输入验证码完毕并点击了提交")
     except:
-        print(time.ctime() +"/r签到失败，没找到提交面板")
+        logger.error(time.ctime() +"/r签到失败，没找到提交面板")
         sendError(time.ctime() + "/r签到失败，没找到提交面板")
     else:
-        print(time.ctime()+"/签到成功")
+        logger.info(time.ctime()+"/签到成功")
 
     driver.quit()
-
-def dosth():
-    print(time.ctime()+"/woooeee")
 
 #多个目的邮箱是string用逗号分割;mail_coding接收的类别为，明文传输（default，25端口）,SSL加密（SSL，465端口），SSL加密（SSL），STARTTLS加密（TLS，587端口）
 def sendError(sendcontent:str):
@@ -239,40 +250,44 @@ def sendError(sendcontent:str):
     try:
         with open(smtpjson,'r',encoding='utf-8') as file:
             mymail = json.load(file)
-            print(time.ctime()+"/获取邮件配置文件成功")
+            logger.info(time.ctime()+"/获取邮件配置文件成功")
         maildriver = mysmtp.Smtpmailserver(mymail)
     except:
-        print(time.ctime()+"/未设定邮箱或邮箱格式出错")
+        logger.warning(time.ctime()+"/未设定邮箱或邮箱格式出错")
         return
     maildriver.Sendmail(sendcontent)
-    print("通知邮件发送成功")
+    logger.info("通知邮件发送成功")
     
 def do_schedule():
-    print("正在运行计划任务, 每天签到一次")
+    logger.info("正在运行计划任务, 每天签到一次")
 
     while True:
-        print("距离下一次任务的时间: {:.1f}分钟".format(schedule.idle_seconds()), end='\r')
+        logger.info("距离下一次任务的时间: {:.1f}分钟".format(schedule.idle_seconds()), end='\r')
         schedule.run_pending()  # 运行所有可以运行的任务
         time.sleep(30)
 
 
 if __name__ == '__main__':
-    print("脚本已启动")
-    #action_group = parser.add_mutually_exclusive_group()
     global mymail   
+    global logger
+    logger = setLogger()
+    logger.info("脚本已启动")
+    #action_group = parser.add_mutually_exclusive_group()
+    
+    
     #时间计划
     #schedule.every(1).minutes.do(job)
     configpath = "./data/config.json"
     try:
         with open(configpath,'r',encoding='utf-8') as file:
             myconfig = json.load(file)
-            print(time.ctime()+"/获取配置文件成功")
+            logger.info(time.ctime()+"/获取配置文件成功")
     except:
-        print(time.ctime()+"/获取配置文件失败，ERROR")
+        logger.error(time.ctime()+"/获取配置文件失败，ERROR")
         
     
     #job(myconfig["usr"],myconfig["pwd"],myconfig["headless"],myconfig["browser"],myconfig["webaddr"])
-    print("签到时间:" + myconfig["time"])
+    logger.info("签到时间:" + myconfig["time"])
     schedule.every().day.at(myconfig["time"]).do(job,myconfig["usr"],myconfig["pwd"],myconfig["headless"],myconfig["browser"],myconfig["webaddr"])
     do_schedule()
     
